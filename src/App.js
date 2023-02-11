a9b0a4edae5ac860ac8334e5a11b62992de5cc2d
Adrian Dworak <adrian.dworak97@gmail.com>
2023-02-11 11:22:46 +0100

    test_commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Adrian Dworak React Github Pages</p>
+        <p>Adrian Dworak lallalalala</p>
         <a
           className="App-link"
           href="https://reactjs.org"
